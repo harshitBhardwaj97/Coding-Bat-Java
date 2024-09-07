19b375e8bfea3ea4698849234a111f3ecf8ebb8c
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-09-07 19:30:56 +0530

    Warmup-1 > diff21 solution added

---
+++ src/main/java/com/harshitbhardwaj/solutions/warmup1/Diff21Solution.java
@@ -0,0 +1,23 @@
+package com.harshitbhardwaj.solutions.warmup1;
+
+public class Diff21Solution {
+
+    /*
+    Given an int n, return the absolute difference between n and 21,
+    except return double the absolute difference if n is over 21.
+     */
+    public static int diff21(int n) {
+        return n > 21 ? 2 * Math.abs(n - 21) : Math.abs(n - 21);
+
+        /*
+        if (n > 21) {
+            return 2 * Math.abs(n - 21);
+        } else {
+            return Math.abs(n - 21);
+        }
+         */
+
+    }
+
+}
+ 
---
+++ src/test/java/com/harshitbhardwaj/tests/warmup1/Diff21Tests.java
@@ -0,0 +1,30 @@
+package com.harshitbhardwaj.tests.warmup1;
+
+import com.harshitbhardwaj.solutions.warmup1.Diff21Solution;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Diff21Tests {
+
+    /*
+    diff21(19) → 2
+    diff21(10) → 11
+    diff21(21) → 0
+     */
+
+    @Test
+    public void diff21TestOne() {
+        Assert.assertEquals(Diff21Solution.diff21(19), 2);
+    }
+
+    @Test
+    public void diff21TestTwo() {
+        Assert.assertEquals(Diff21Solution.diff21(10), 11);
+    }
+
+    @Test
+    public void diff21TestThree() {
+        Assert.assertEquals(Diff21Solution.diff21(21), 0);
+    }
+}
+ 
