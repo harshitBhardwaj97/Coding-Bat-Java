f7962f64560c577e0cf42d38b9091e21e33f1da4
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-09-26 22:25:25 +0530

    Warmup-1 > lastDigit URL and description added

--- src/main/java/com/harshitbhardwaj/solutions/warmup1/LastDigitSolution.java
+++ src/main/java/com/harshitbhardwaj/solutions/warmup1/LastDigitSolution.java
@@ -1,6 +1,11 @@
 package com.harshitbhardwaj.solutions.warmup1;
 
+// https://codingbat.com/prob/p125339
 public class LastDigitSolution {
+
+    /*
+    Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57.
+     */
     public static boolean lastDigit(int a, int b) {
         int lastDigitOfA = a % 10;
         int lastDigitOfB = b % 10;
