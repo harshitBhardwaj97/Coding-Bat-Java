4132e5cda2034a6fbbce6b294ef6b0239b0be5a4
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-09-29 20:17:38 +0530

    Array1 > firstLast6 description added

--- src/main/java/com/harshitbhardwaj/solutions/array1/FirstLast6Solution.java
+++ src/main/java/com/harshitbhardwaj/solutions/array1/FirstLast6Solution.java
@@ -3,6 +3,10 @@ package com.harshitbhardwaj.solutions.array1;
 // https://codingbat.com/prob/p185685
 public class FirstLast6Solution {
 
+    /*
+    Given an array of ints, return true if 6 appears as either the first or last element in the array.
+    The array will be length 1 or more.
+     */
     public static boolean firstLast6(int[] nums) {
         int firstElement = nums[0];
         int lastElement = nums[nums.length - 1];
